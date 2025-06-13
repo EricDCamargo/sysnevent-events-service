d626f067dafe859e7f05c15f246040210c7c62b6
EricDCamargo <ericdcamargo@gmail.com>
2025-06-13 16:55:20 -0300

    build: new build

--- dist/@types/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["DOCENT_ASSISTANT"] = "DOCENT_ASSISTANT";
-    Role["COORDINATOR"] = "COORDINATOR";
-    Role["ADMIN"] = "ADMIN";
-})(Role || (exports.Role = Role = {}));
